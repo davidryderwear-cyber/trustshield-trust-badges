bde0634883230c853e4dc65e38cbf89e87841eef
davidryderwear-cyber <david.ryderwear@gmail.com>
2026-04-05 20:59:43 +0930

    Security and scope fixes from audit - remove debug route, add read_products scope, cleanup temp files

--- app/routes/debug.jsx
+++
@@ -1,9 +0,0 @@
-import { json } from "@remix-run/node";
-
-export const loader = async () => {
-  return json({
-    SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL || "(not set)",
-    SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY ? "set (" + process.env.SHOPIFY_API_KEY.substring(0, 8) + "...)" : "(not set)",
-    NODE_ENV: process.env.NODE_ENV || "(not set)",
-  });
-};
--- run-gh.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Program Files\GitHub CLI
-gh auth login -p https -h github.com -w
